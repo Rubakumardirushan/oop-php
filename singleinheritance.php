c7a9ae3ccc7222db6b9ec048df2734b553e6a8ef
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-03-17 12:54:56 +0530

    method overriding

--- singleinheritance.php
+++ singleinheritance.php
@@ -2,25 +2,27 @@
 
 class car{
     function driver(){
-        echo "I am driving car";
+        echo "I am driving car" . PHP_EOL;
     }
 }
-class electic_car extends car{
-    function battery(){
-        echo "I am using battery";
-    }
 
+class electric_car extends car{
+    // Method overriding: Overrides the driver() method of the parent class
+    function driver(){
+        // Calls the driver() method of the parent class
+        parent::driver();
+        echo "I am driving electric car" . PHP_EOL;
+    }
+    
+    function battery(){
+        echo "I am using battery" . PHP_EOL;
+    }
 }
-/*
-$tesla= new electic_car();
+
+$tesla = new electric_car();
 $tesla->driver();
 $tesla->battery();
-$normalcar= new car();
-$normalcar->driver();
-*/
 
-
-
-
-
+$normal_car = new car();
+$normal_car->driver();
 ?>

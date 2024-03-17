e2851e21ca3e5a857d169ac1bc302f1f0b42906a
Rubakumardirushan <dirusha-se19033@stu.kln.ac.lk>
2024-03-17 11:44:34 +0530

    oop-php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+ class car{
+     public $carname;
+     function set_name($carname){
+         $this->carname=$carname;
+     }
+     function get_carname(){
+         return $this->carname;
+     }
+ }
+ $car1= new car();
+ $car1->set_name('black');
+
+ echo ( $car1->get_carname());
+?>
